2cf1a5a2fa4f6009207bce1aef6c0504fa4630e8
Felipe Dc <felipedc@Mac-mini-de-Felipedc.local>
2018-04-30 19:47:02 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,74 @@
+// @flow
+import * as React from "react";
+import {StatusBar, Platform} from "react-native";
+import {useStrict, observable, action} from "mobx";
+import {Provider, observer} from "mobx-react/native";
+import {StackNavigator} from "react-navigation";
+import {Font, AppLoading} from "expo";
+import {Feather} from "@expo/vector-icons";
+import ModalHost from "expo/src/modal/ModalHost";
+
+import {Images, createTheme} from "./src/components";
+import {StackNavigatorOptions} from "./src/components/Navigation";
+
+import {Welcome} from "./src/welcome";
+import {FoodNavigator} from "./src/food";
+import {SocialNavigator} from "./src/social";
+import {MusicNavigator} from "./src/music";
+import {PhotographyNavigator} from "./src/photography";
+import {TravelNavigator} from "./src/travel";
+import {Player} from "./src/music/components";
+
+const SFProTextBold = require("./fonts/SF-Pro-Text-Bold.otf");
+const SFProTextSemibold = require("./fonts/SF-Pro-Text-Semibold.otf");
+const SFProTextRegular = require("./fonts/SF-Pro-Text-Regular.otf");
+
+useStrict(true);
+
+const onNavigationStateChange = () => undefined;
+
+@observer
+export default class App extends React.Component<{}> {
+
+    @observable isReady = false;
+    @action ready() { this.isReady = true; }
+
+    async componentDidMount(): Promise<void> {
+        StatusBar.setBarStyle("dark-content");
+        if (Platform.OS === "android") {
+            StatusBar.setBackgroundColor("white");
+        }
+        const fonts = Font.loadAsync({
+            "SFProText-Bold": SFProTextBold,
+            "SFProText-Semibold": SFProTextSemibold,
+            "SFProText-Regular": SFProTextRegular
+        });
+        const images = Images.downloadAsync();
+        const icons = Font.loadAsync(Feather.font);
+        await Promise.all([fonts, ...images, icons]);
+        this.ready();
+    }
+
+    render(): React.Node {
+        const {isReady} = this;
+        if (!isReady) {
+            return <AppLoading />;
+        }
+        return (
+            <Provider theme={createTheme()} player={new Player()}>
+                <ModalHost>
+                    <MainNavigator {...{onNavigationStateChange}} />
+                </ModalHost>
+            </Provider>
+        );
+    }
+}
+
+const MainNavigator = StackNavigator({
+    Welcome: { screen: Welcome },
+    Food: { screen: FoodNavigator },
+    Social: { screen: SocialNavigator },
+    Music: { screen: MusicNavigator },
+    Photography: { screen: PhotographyNavigator },
+    Travel: { screen: TravelNavigator }
+}, StackNavigatorOptions);
---
+++ src/welcome/Welcome.js
@@ -0,0 +1,136 @@
+// @flow
+import autobind from "autobind-decorator";
+import * as React from "react";
+import {SafeAreaView, ScrollView, StyleSheet, View, Image, StatusBar, Platform} from "react-native";
+
+import {NavigationHelpers, Colors, StyleGuide, Images, Text, withTheme} from "../components";
+
+import Kit from "./Kit";
+
+import type {ThemeProps, ThemeName} from "../components/theme";
+import type {NavigationProps} from "../components/Navigation";
+
+const images = require("./images");
+
+class Welcome extends React.Component<ThemeProps & NavigationProps<>> {
+
+    componentDidMount() {
+        StatusBar.setBarStyle("dark-content");
+        if (Platform.OS === "android") {
+            StatusBar.setBackgroundColor("white");
+        }
+    }
+
+    navigate(themeName: ThemeName) {
+        const { navigation, theme } = this.props;
+        StatusBar.setBarStyle("light-content");
+        if (Platform.OS === "android") {
+            StatusBar.setBackgroundColor(Colors[themeName].primary);
+        }
+        theme.switchColors(Colors[themeName]);
+        NavigationHelpers.reset(navigation, themeName);
+    }
+
+    @autobind
+    food() {
+        this.navigate("Food");
+    }
+
+    @autobind
+    social() {
+        this.navigate("Social");
+    }
+
+    @autobind
+    music() {
+        this.navigate("Music");
+    }
+
+    @autobind
+    photography() {
+        this.navigate("Photography");
+    }
+
+    @autobind
+    travel() {
+        this.navigate("Travel");
+    }
+
+    render(): React.Node {
+        return (
+            <View style={styles.container}>
+                <SafeAreaView style={styles.safeHeader}>
+                    <View style={styles.header}>
+                        <View>
+                            <Text type="footnote">SKETCH ELEMENTS</Text>
+                            <Text type="title1">Apps</Text>
+                        </View>
+                        <Image source={Images.logo} style={styles.logo} />
+                    </View>
+                </SafeAreaView>
+                <ScrollView contentContainerStyle={styles.content}>
+                    <Kit
+                        uri={images.food.uri}
+                        preview={images.food.preview}
+                        title="Food"
+                        backgroundColor={Colors.Food.primary}
+                        onPress={this.food}
+                    />
+                    <Kit
+
+                        uri={images.social.uri}
+                        preview={images.social.preview}
+                        title="Social"
+                        backgroundColor={Colors.Social.primary}
+                        onPress={this.social}
+                    />
+                    <Kit
+                        uri={images.music.uri}
+                        preview={images.music.preview}
+                        title="Music"
+                        backgroundColor={Colors.Music.primary}
+                        onPress={this.music}
+                    />
+                    <Kit
+                        uri={images.photography.uri}
+                        preview={images.photography.preview}
+                        title="Photography"
+                        backgroundColor={Colors.Photography.primary}
+                        onPress={this.photography}
+                    />
+                    <Kit
+                        uri={images.travel.uri}
+                        preview={images.travel.preview}
+                        title="Travel"
+                        backgroundColor={Colors.Travel.primary}
+                        onPress={this.travel}
+                    />
+                </ScrollView>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    safeHeader: {
+        ...StyleGuide.styles.shadow
+    },
+    header: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: StyleGuide.spacing.small
+    },
+    logo: {
+        width: 50,
+        height: 50
+    },
+    content: {
+        paddingVertical: StyleGuide.spacing.small
+    }
+});
+
+export default withTheme(Welcome);
